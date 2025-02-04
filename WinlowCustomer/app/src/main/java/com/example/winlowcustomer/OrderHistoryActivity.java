b8d96e48094bdeec12ce3fb7b83f4c3641e30afc
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-02-04 16:18:56 +0530

    order history

--- WinlowCustomer/app/src/main/java/com/example/winlowcustomer/OrderHistoryActivity.java
+++ WinlowCustomer/app/src/main/java/com/example/winlowcustomer/OrderHistoryActivity.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterStep_2_Activity extends AppCompatActivity {
+public class OrderHistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register_step2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cordinatorLayout), (v, insets) -> {
+        setContentView(R.layout.activity_order_history);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
