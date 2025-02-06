b013c0de3aed122cfc9be496644820dccc7151c1
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-02-06 08:03:24 +0530

    connection

--- WinlowCustomer/app/src/main/java/com/example/winlowcustomer/HomeActivity.java
+++ WinlowCustomer/app/src/main/java/com/example/winlowcustomer/HomeActivity.java
@@ -1,12 +1,29 @@
 package com.example.winlowcustomer;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.winlowcustomer.modal.NetworkConnection;
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.search.SearchBar;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,5 +37,60 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // check network connection
+        NetworkConnection.register(getApplicationContext());
+
+        // click search
+        SearchBar searchbar = findViewById(R.id.searchBar);
+        searchbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                changeActivity(SearchProductActivity);
+
+            }
+        });
+
+        // click profile image
+        ImageView profileImage = findViewById(R.id.imageView);
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                changeActivity(ProfileActivity);
+
+            }
+        });
+
+        // click location
+        TextView selectLocation = findViewById(R.id.textView4);
+        selectLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                changeActivity(SelectLocationActivity);
+            }
+        });
+
+        // change category
+        ChipGroup categoryChipGroup = findViewById(R.id.categoryChipGroup);
+        categoryChipGroup.setOnCheckedStateChangeListener(new ChipGroup.OnCheckedStateChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull ChipGroup group, @NonNull List<Integer> checkedIds) {
+
+                if(checkedIds.contains("chipAll")){
+                    loadProduct("a");
+                }
+
+            }
+        });
+
     }
+
+    private void changeActivity(){
+        Intent intent = new Intent(HomeActivity.this, activity);
+        startActivity(intent);
+    }
+
 }
---
+++ WinlowCustomer/app/src/main/java/com/example/winlowcustomer/modal/NetworkConnection.java
@@ -0,0 +1,68 @@
+package com.example.winlowcustomer.modal;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+
+import com.example.winlowcustomer.R;
+import com.google.android.material.snackbar.Snackbar;
+
+public class NetworkConnection {
+
+    private static AlertDialog alertDialog;
+
+    public static void register(Context context){
+
+        ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback(){
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                super.onAvailable(network);
+
+                if (alertDialog != null && alertDialog.isShowing()) {
+                    alertDialog.dismiss();
+                }
+
+                Toast.makeText(context, R.string.connection_available,Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onLost(@NonNull Network network) {
+                super.onLost(network);
+                Toast.makeText(context, R.string.connection_lost,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onUnavailable() {
+                super.onUnavailable();
+                if (alertDialog == null) {
+                    alertDialog = new AlertDialog.Builder(context)
+                            .setTitle(R.string.connection_unavailable_title)
+                            .setMessage(R.string.connection_unavailable)
+                            .create();
+                }
+
+                if (!alertDialog.isShowing()) {
+                    alertDialog.show();
+                }
+            }
+        };
+
+        NetworkRequest networkRequest = new NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build();
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(ConnectivityManager.class);
+        connectivityManager.registerNetworkCallback(networkRequest,networkCallback);
+
+    }
+
+}
