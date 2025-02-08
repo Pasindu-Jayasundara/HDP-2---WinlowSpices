402d348fb7f942ab6b6116c15d7d0fa5513a4546
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-02-09 00:30:02 +0530

    single product

--- WinlowCustomer/app/src/main/java/com/example/winlowcustomer/ProductViewActivity.java
+++ WinlowCustomer/app/src/main/java/com/example/winlowcustomer/ProductViewActivity.java
@@ -1,6 +1,12 @@
 package com.example.winlowcustomer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.example.winlowcustomer.dto.ProductDTO;
+
 public class ProductViewActivity extends AppCompatActivity {
+
+    ProductDTO productDTO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,51 @@ public class ProductViewActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = new Intent();
+        productDTO = (ProductDTO) intent.getSerializableExtra("productDTO");
+
+        // load product data
+        TextView productTitle = findViewById(R.id.textView5);
+        TextView productName = findViewById(R.id.textView6);
+        TextView productCategory = findViewById(R.id.textView7);
+        TextView productDiscount = findViewById(R.id.textView59);
+
+        // image load
+        ImageView imageView = findViewById(R.id.imageView2);
+        Glide.with(this) // Use context
+                .load(productDTO.getImagePath()) // Load image URL
+                .placeholder(R.drawable.product_placeholder) // Optional: placeholder image
+                .error(R.drawable.product_placeholder) // Optional: error image
+                .into(imageView); // Set image into ImageView
+
+        productTitle.setText(productDTO.getName());
+        productName.setText(productDTO.getName());
+        productCategory.setText(productDTO.getCategory());
+        if (productDTO.getDiscount() > 0) {
+            productDiscount.setVisibility(View.VISIBLE);
+            String discountTxt = String.valueOf(productDTO.getDiscount()) + "% Off";
+            productDiscount.setText(discountTxt);
+        } else {
+            productDiscount.setVisibility(View.GONE);
+        }
+
+        // back button
+        ImageButton backButton = findViewById(R.id.imageButton);
+        ImageButton backButton2 = findViewById(R.id.imageButton3);
+        backButton.setOnClickListener(v -> finish());
+        backButton2.setOnClickListener(v -> finish());
+
+        // add to cart
+        Button addToCart = findViewById(R.id.button3);
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
     }
 }
---
+++ WinlowCustomer/app/src/main/java/com/example/winlowcustomer/modal/SingleProductViewRecyclerViewAdapter.java
@@ -0,0 +1,97 @@
+package com.example.winlowcustomer.modal;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.winlowcustomer.R;
+import com.example.winlowcustomer.dto.WeightCategoryDTO;
+
+import java.util.ArrayList;
+
+public class SingleProductViewRecyclerViewAdapter extends RecyclerView.Adapter<SingleProductViewRecyclerViewAdapter.SingleProductViewRecyclerViewHolder>{
+
+    ArrayList<WeightCategoryDTO> weightCategoryDTOArrayList;
+
+    public class SingleProductViewRecyclerViewHolder extends RecyclerView.ViewHolder{
+
+        TextView weight;
+        TextView weightUnitPrice;
+        TextView selectedQty;
+        ImageButton weightMinus;
+        ImageButton weightPlus;
+
+        public SingleProductViewRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            weight = itemView.findViewById(R.id.textView60);
+            weightUnitPrice = itemView.findViewById(R.id.textView61);
+            selectedQty = itemView.findViewById(R.id.textView62);
+            weightMinus = itemView.findViewById(R.id.imageButton25);
+            weightPlus = itemView.findViewById(R.id.imageButton24);
+        }
+    }
+
+    public SingleProductViewRecyclerViewAdapter(ArrayList<WeightCategoryDTO> weightCategoryDTOArrayList) {
+        this.weightCategoryDTOArrayList = weightCategoryDTOArrayList;
+    }
+
+    @NonNull
+    @Override
+    public SingleProductViewRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View card = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_weight_category_layout, parent, false);
+
+        return new SingleProductViewRecyclerViewHolder(card);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SingleProductViewRecyclerViewHolder holder, int position) {
+
+        WeightCategoryDTO weightCategoryDTO = weightCategoryDTOArrayList.get(position);
+
+        String weight = String.valueOf(weightCategoryDTO.getWeight())+ (weightCategoryDTO.getWeight()>1000?"Kg":"g");
+        holder.weight.setText(weight);
+
+        String unitPrice = "Rs. "+ String.valueOf(weightCategoryDTO.getUnitPrice());
+        holder.weightUnitPrice.setText(unitPrice);
+
+        holder.selectedQty.setText(0);
+
+        holder.weightMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int qty = Integer.parseInt(holder.selectedQty.getText().toString());
+                if(qty>0){
+                    qty--;
+                    holder.selectedQty.setText(String.valueOf(qty));
+                }
+
+            }
+        });
+
+        holder.weightPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int qty = Integer.parseInt(holder.selectedQty.getText().toString());
+                qty++;
+                holder.selectedQty.setText(String.valueOf(qty));
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return weightCategoryDTOArrayList.size();
+    }
+
+}
