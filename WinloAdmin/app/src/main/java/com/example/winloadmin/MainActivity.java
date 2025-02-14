74654cb93f3eef749cd56abced26f2e951cc4d3f
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-02-15 01:46:47 +0530

    product

---
+++ WinloAdmin/app/src/main/java/com/example/winloadmin/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.winloadmin;
+
+import static android.view.Gravity.START;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.winloadmin.nav.ProductFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.inflateMenu(R.menu.navigation_menu);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+                if (item.getItemId() == R.id.productFragment) {
+                    item.setChecked(true); // Highlight selected item
+                    navigationView.getMenu().getItem(0).setChecked(true);
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayout, new ProductFragment())
+                            .setReorderingAllowed(true)
+                            .commit();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+
+
+
+    }
+}
---
+++ WinloAdmin/settings.gradle.kts
@@ -0,0 +1,28 @@
+import org.gradle.internal.impldep.org.bouncycastle.oer.its.template.etsi102941.EtsiTs102941TrustLists.Url
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+rootProject.name = "WinloAdmin"
+include(":app")
