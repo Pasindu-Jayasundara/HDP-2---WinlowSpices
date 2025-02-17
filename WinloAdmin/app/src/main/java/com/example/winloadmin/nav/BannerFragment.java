ae157946a82af8eead75474587a5170221ab7e21
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-02-17 16:04:28 +0530

    banner

---
+++ WinloAdmin/app/src/main/java/com/example/winloadmin/nav/BannerFragment.java
@@ -0,0 +1,31 @@
+package com.example.winloadmin.nav;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.winloadmin.R;
+
+public class BannerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_banner, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+}
